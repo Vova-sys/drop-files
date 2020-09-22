d678fd103c22b25845c0e34603c94664f27e4b3b
v.dedkov <d3dkof@gmail.com>
2020-09-22 15:45:30 +0300

    first commit

--- loadfiles/dimaboss/models.py
+++ loadfiles/dimaboss/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 
 class Router(models.Model):
+    class Meta:
+        verbose_name = 'Файлы'
+        verbose_name_plural = 'Загрузка файлов'
+
     specifications = models.FileField(upload_to='router_specifications')
